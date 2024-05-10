d23c8e0a5beb5261ac77aec9f943b0223d130603
viktor.boichenko <vv.boichenko.vv@gmail.com>
2024-05-10 11:51:06 +0200

    Initial commit

---
+++ src/main/java/pl/lodz/sii/promocodeapi/PromocodeApiApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.sii.promocodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocodeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromocodeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lodz/sii/promocodeapi/PromocodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.sii.promocodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromocodeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
