ecdd8ffa9dec49b90edd8bfb669e0d223b7defb1
viktor.boichenko <vv.boichenko.vv@gmail.com>
2024-05-10 19:26:17 +0200

    Purchase model

--- src/main/java/pl/lodz/sii/promocodeapi/core/model/Purcase.java
+++ src/main/java/pl/lodz/sii/promocodeapi/core/model/Purcase.java
@@ -4,8 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import pl.lodz.sii.promocodeapi.core.exception.PromoCodeException;
 import pl.lodz.sii.promocodeapi.core.exception.ValidationException;
-
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -16,10 +16,32 @@ import java.util.Optional;
 public class Purcase implements Validatable {
     Long id;
     LocalDate date;
-    PromoCode product;
+    Product product;
     Optional<PromoCode> promoCode;
     Price regularPrice;
     Price discountPrice;
+
+    public Purcase(Product product) {
+        this.date = LocalDate.now();
+        this.product = product;
+        this.promoCode = Optional.empty();
+        this.regularPrice = product.getPrice();
+        this.discountPrice = product.getPrice();
+    }
+
+    public Purcase(Product product, PromoCode promoCode) {
+        this.date = LocalDate.now();
+        this.product = product;
+        this.promoCode = Optional.of(promoCode);
+        this.regularPrice = product.getPrice();
+        if (this.promoCode.isPresent()) {
+            try {
+                this.discountPrice = promoCode.applyTo(product);
+            } catch (PromoCodeException e) {
+                this.discountPrice = this.regularPrice;
+            }
+        }
+    }
 
     @Override
     public void validate() throws ValidationException {
