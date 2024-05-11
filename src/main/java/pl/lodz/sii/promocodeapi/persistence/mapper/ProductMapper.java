10ed1bcf14c3de7a33bd8920f2496bcc33787932
viktor.boichenko <vv.boichenko.vv@gmail.com>
2024-05-11 16:14:54 +0200

    persistence ProductMapper

---
+++ src/main/java/pl/lodz/sii/promocodeapi/persistence/mapper/ProductMapper.java
@@ -0,0 +1,34 @@
+package pl.lodz.sii.promocodeapi.persistence.mapper;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import pl.lodz.sii.promocodeapi.core.exception.ValidationException;
+import pl.lodz.sii.promocodeapi.core.model.Currency;
+import pl.lodz.sii.promocodeapi.core.model.Price;
+import pl.lodz.sii.promocodeapi.core.model.Product;
+import pl.lodz.sii.promocodeapi.persistence.entity.ProductEntity;
+import java.math.BigDecimal;
+
+public class ProductMapper implements ModelEntityMapper<Product, ProductEntity> {
+
+    @Override
+    public ProductEntity toEntity(Product product) {
+        Long id = product.getId();
+        String name = product.getName();
+        BigDecimal price = product.getPrice().getValue();
+        Currency currency = product.getPrice().getCurrency();
+        return new ProductEntity(id, name, price, currency);
+    }
+
+    @Override
+    public Product toModel(ProductEntity entity) throws ValidationException {
+        Product product = new Product();
+        product.setId(entity.getId());
+        product.setName(entity.getName());
+        Price productPrice = new Price(entity.getPrice(), entity.getCurrency());
+        product.setPrice(productPrice);
+
+        product.validate();
+        return product;
+    }
+}
