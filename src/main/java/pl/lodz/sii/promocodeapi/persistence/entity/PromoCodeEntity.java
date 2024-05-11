2180291ee27de4423cad3e541a8350f9be793798
viktor.boichenko <vv.boichenko.vv@gmail.com>
2024-05-11 17:52:03 +0200

    PromoCodeEntity

---
+++ src/main/java/pl/lodz/sii/promocodeapi/persistence/entity/PromoCodeEntity.java
@@ -0,0 +1,24 @@
+package pl.lodz.sii.promocodeapi.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pl.lodz.sii.promocodeapi.core.model.Currency;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "promo_codes")
+@Data
+@NoArgsConstructor
+public class PromoCodeEntity {
+    @Id
+    private String code;
+    LocalDate expires;
+    BigDecimal value;
+    @Enumerated(EnumType.STRING)
+    Currency currency;
+    Integer maximumUsages;
+    Integer hasBeenUsedTimes;
+}
