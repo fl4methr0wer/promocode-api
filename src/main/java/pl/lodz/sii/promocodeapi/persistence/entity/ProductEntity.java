0b960d4913a1ded9fa52bd517186ed32cb962177
viktor.boichenko <vv.boichenko.vv@gmail.com>
2024-05-11 16:10:26 +0200

    ProductEntity persistence

---
+++ src/main/java/pl/lodz/sii/promocodeapi/persistence/entity/ProductEntity.java
@@ -0,0 +1,23 @@
+package pl.lodz.sii.promocodeapi.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pl.lodz.sii.promocodeapi.core.model.Currency;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    Long id;
+    String name;
+    BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    Currency currency;
+}
---
+++ src/main/java/pl/lodz/sii/promocodeapi/persistence/mapper/ModelEntityMapper.java
@@ -0,0 +1,8 @@
+package pl.lodz.sii.promocodeapi.persistence.mapper;
+
+import pl.lodz.sii.promocodeapi.core.exception.ValidationException
+
+public interface ModelEntityMapper<Model, Entity> {
+    Entity toEntity(Model model);
+    Model toModel(Entity entity) throws ValidationException;
+}
